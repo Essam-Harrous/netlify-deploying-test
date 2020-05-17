4e8a20761a8abbf10d867567b1caf42a6edcfc7b
essam <esamfarg91@gmail.com>
2020-05-18 00:30:12 +0200

    try with mongoose

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const mongoose = require('mongoose');
+
+const Test = require('./models/index');
+
+mongoose
+  .connect(
+    'mongodb+srv://essam:1234@test1-jvuqg.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .catch((err) => console.log(err));
+mongoose.connection.once('open', () => console.log('database is connected'));
 
 const app = express();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    hello: 'hiii!',
-  });
+router.get('/', async (req, res) => {
+  const test = await Test.create({ name: 'essam harous' });
+  res.json(test);
 });
 
 app.use('/.netlify/functions/api', router);
